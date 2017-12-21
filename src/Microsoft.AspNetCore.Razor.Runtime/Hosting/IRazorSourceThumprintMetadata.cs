21638096816fbf83ccd3cbaf7f10d7683a27155d
Ryan Nowak <nowakra@gmail.com>
2017-12-20 18:29:28 -0800

    [Design] Add metadata to Razor
    This PR introduces standard metadata to Razor. This change begins to
    formalize the contract between generated code produced by Razor and
    runtimes that want to load and interact with this code.
    
    This is a step on making MVC a 'plugin' to Razor rather than the only
    possible implementation. Since we're doing MSBuild work - this is the
    right time to designate the current interaction between Razor and MVC as
    'legacy' and move forward.
    
    Additionally, we need the source thumbprinting to make re-compilation of
    Razor a thing.

---
+++ src/Microsoft.AspNetCore.Razor.Runtime/Hosting/IRazorSourceThumprintMetadata.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.AspNetCore.Razor.Hosting
+{
+    public interface IRazorSourceThumprintMetadata
+    {
+        string HashAlgorithm { get; }
+
+        string Identifier { get; }
+
+        string Kind { get; }
+        
+        string Thumbprint { get; }
+    }
+}
---
+++ src/Microsoft.AspNetCore.Razor.Runtime/Hosting/RazorSourceThumbprintAttribute.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNetCore.Razor.Hosting
+{
+    /// <summary>
+    /// Specifies the hash thumprint of a source file that contributed to a compiled item.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// These attributes are added by the Razor infrastructure when generating code to assist runtime
+    /// implementations to determine the integrity of compiled items.
+    /// </para>
+    /// <para>
+    /// Runtime implementations should access the thumbprint metadata for an item using
+    /// <see cref="RazorCompiledItemExtensions.GetThumprintMetadata(RazorCompiledItem)"/>.
+    /// </para>
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public sealed class RazorSourceThumbprintAttribute : Attribute, IRazorSourceThumprintMetadata
+    {
+        /// <summary>
+        /// Creates a new <see cref="RazorSourceThumbprintAttribute"/>.
+        /// </summary>
+        /// <param name="hashAlgorithm">The hash algorithm used to create this thumbprint.</param>
+        /// <param name="thumbprint">The thumbprint as a string.</param>
+        /// <param name="kind">The kind of the source file associated with this thumbprint.</param>
+        /// <param name="identifier">The identifier associated with this thumbprint. May be <c>null</c>.</param>
+        public RazorSourceThumbprintAttribute(string hashAlgorithm, string thumbprint, string kind, string identifier)
+        {
+            if (hashAlgorithm == null)
+            {
+                throw new ArgumentNullException(nameof(hashAlgorithm));
+            }
+
+            if (thumbprint == null)
+            {
+                throw new ArgumentNullException(nameof(thumbprint));
+            }
+
+            if (kind == null)
+            {
+                throw new ArgumentNullException(nameof(kind));
+            }
+
+            HashAlgorithm = hashAlgorithm;
+            Thumbprint = thumbprint;
+            Kind = kind;
+            Identifier = identifier;
+        }
+
+        /// <summary>
+        /// Gets the hash algorithm used to create this thumbprint.
+        /// </summary>
+        public string HashAlgorithm { get; }
+        
+        /// <summary>
+        /// Gets the identifier of the source file associated with this thumbprint. May be <c>null</c>.
+        /// </summary>
+        public string Identifier { get; }
+
+        /// <summary>
+        /// Gets the kind of the source file associated with this thumbprint.
+        /// </summary>
+        public string Kind { get; }
+
+        /// <summary>
+        /// Gets the thumbprint as string.
+        /// </summary>
+        public string Thumbprint { get; }
+    }
+}
